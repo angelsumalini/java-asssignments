f3eb0b96b9402d9cf6d7cd83db4d63c525b3ddef
angelsumalini <70747259+angelsumalini@users.noreply.github.com>
2021-07-07 06:56:15 +0530

    Add files via upload

---
+++ minmax.java
@@ -0,0 +1,19 @@
+public class minmax {
+    public static void main(String [] args) {
+        int arr[] = {3,2,1,56,10000,167};
+        int min = arr[0];
+        int max = arr[0];
+        
+        for(int i =0; i<arr.length; i++) {
+            
+            if(arr[i] < min) {
+                min = arr[i];
+            }
+            
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+           }
+System.out.println("min" + " " + min + "max" + " " + max);
+    }
+}
