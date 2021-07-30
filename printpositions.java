f6c61bc1f3bde3fef03f597c09b493400a9de009
angelsumalini <70747259+angelsumalini@users.noreply.github.com>
2021-07-30 10:31:27 +0530

    Add files via upload

---
+++ printpositions.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class printpositions
+{
+public static void main(String args[]) {
+int arr[] = new int[] {10,20,30,40,50,60,70,80};
+
+for(int i=0; i<arr.length;i=i+2){
+System.out.println("index = " + i + "," +"value =" + arr[i]);
+}
+System.out.println("");
+for(int i=1;i<arr.length;i=i+2) {
+System.out.println("index = " + i + "," + "value =" + arr[i]);
+if(
+}
+}
+}
